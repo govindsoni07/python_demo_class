3c3b25a34de5b2334b71c46a8c0f77c4b4806a07
Govind <govindsoniii07@gmail.com>
2026-09-11 11:06:43 +0530

    updated code

--- grade.py
+++ grade.py
@@ -1,13 +1,15 @@
-score=int(input("Enter score : "))
-if score>=90 and score<=100:
-    print("O Grade")
-elif score>=80 and score<=90:
-    print("A Grade")
-elif score>=65 and score<=80:
-    print("B Grade")
-elif score>35 and score<=65:
-    print("C Grade")
-elif score<=35:
-    print("F Grade")
+score = int(input("Enter score: "))
+
+if score >= 1 and score <= 100:
+    if score >= 90:
+        print("O Grade")
+    elif score >= 80:
+        print("A Grade")
+    elif score >= 65:
+        print("B Grade")
+    elif score > 35:
+        print("C Grade")
+    else:
+        print("F Grade")
 else:
     print("Invalid")
---
+++ numberCheck.py
@@ -0,0 +1,7 @@
+n=int(input("Enter a no. to check: "))
+if n==0:
+    print("Zero")
+elif n<1:
+    print("Negative")
+else:
+    print("Positive")
