01190b321705e5106bac0f54ad3bcd6216ac19b0
Govind <govindsoniii07@gmail.com>
2026-09-17 10:24:04 +0530

    add more pgm

---
+++ fun_calc.py
@@ -0,0 +1,20 @@
+def add(a,b):
+    return a+b
+def sub(a,b):
+    return a-b
+def mul(a,b):
+    return a*b
+def div(a,b):
+    return a/b
+def mod(a,b):
+    return a%b
+def flord(a,b):
+    return a//b
+a=int(input("Enter 1st no. : "))
+b=int(input("Enter 2nd no. : "))
+print(add(a,b))
+print(sub(a,b))
+print(mul(a,b))
+print(div(a,b))
+print(mod(a,b))
+print(flord(a,b))
