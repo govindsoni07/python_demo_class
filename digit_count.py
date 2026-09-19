1491ac27727242208bc29e285f1e85376ab4a472
Govind <govindsoniii07@gmail.com>
2026-09-19 14:04:09 +0530

    digti count

--- digit_count.py
+++ digit_count.py
@@ -1,9 +1,11 @@
-n=int(input("Enter a no. "))
-count=0
-if n==0:
-    print("1")
+n = int(input("Enter a no. "))
+count = 0
+
+if n == 0:
+    print(1)
 else:
-    while(n>0):
-        count+=1
-        n//=10
-print(count)
+    while n > 0:
+        count += 1
+        n //= 10
+
+    print(count)
