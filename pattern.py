f46eb3b0068323fe645194f6ff5e34874650aac5
Govind <govindsoniii07@gmail.com>
2026-09-21 10:34:38 +0530

    more pattern added

--- pattern.py
+++ pattern.py
@@ -89,11 +89,27 @@ for i in range(1, n + 1):          # Outer loop for rows
     
     # Inner Loop 1: Print leading spaces
     for j in range(n - i):
-        print(" ", end="")
+        print(" ", end=" ")
         
     # Inner Loop 2: Print stars
     for k in range(2 * i- 1):
-        print("*", end="")
+        print("*", end=" ")
+        
+    # Move to the next line after completing the row
+    print()
+
+print("----------------------new pattern ulta----------------")
+n = 5
+
+for i in range(n,0,-1):          # Outer loop for rows
+    
+    # Inner Loop 1: Print leading spaces
+    for j in range(n - i):
+        print(" ", end=" ")
+        
+    # Inner Loop 2: Print stars
+    for k in range(2 * i- 1):
+        print("*", end=" ")
         
     # Move to the next line after completing the row
     print()
